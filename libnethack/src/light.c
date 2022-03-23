6d3244ffb61fff068c3baf80adc711976d991a67
Alex Smith <ais523@nethack4.org>
2022-03-23 00:40:49 +0000

    Convert lightsource-transfer bug from crash to panic
    To make testing easier. This doesn't fix the bug, but produces a
    better symptom when it occurs.

--- libnethack/src/light.c
+++ libnethack/src/light.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2015-07-21 */
+/* Last modified by Alex Smith, 2022-03-23 */
 /* Copyright (c) Dean Luick, 1994                                       */
 /* NetHack may be freely redistributed.  See license for details.       */
 
@@ -212,6 +212,8 @@ transfer_lights(struct level *oldlev, struct level *newlev, unsigned int obj_id)
 
     if (newlev == oldlev)
         return;
+    if (newlev == NULL || oldlev == NULL)
+        panic("Attempting to transfer lights to/from NULL");
 
     for (prev = &oldlev->lev_lights; (curr = *prev) != 0;) {
         switch (curr->type) {
